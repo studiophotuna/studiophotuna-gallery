b2725da9414e203138e060b122e87a91a1741102
studiophotuna <studiophotuna@gmail.com>
2026-04-04 00:50:37 +0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "elthktbvojsmvhtxxqnz.supabase.co",
-      },
-    ],
-  },
   reactCompiler: true,
 };
 
